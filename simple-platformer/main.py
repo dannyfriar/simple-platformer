194db0cccc53b2e9cbfe701b2bf771f8797be7b3
dannyfriar <dannyfriar@hotmail.co.uk>
2016-05-13 09:58:03 +0100

    Simple platformer PyGame initial commit@

---
+++ simple-platformer/main.py
@@ -0,0 +1,190 @@
+import pygame
+import math
+import random
+import time
+from pygame.locals import *
+from player import *
+from block import *
+from camera import *
+from token import *
+
+##---- SETUP PYGAME -----##
+from config import *
+
+## Initialise pygame
+pygame.init()
+window = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+pygame.display.set_caption("Platformer")
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, FONT_SIZE)
+
+
+##---- CAMERA METHOD - considering including in Camera class ----## 
+def complex_camera(camera, target_rect):
+	l, _, _, _ = target_rect 					 # l = left
+	_, t, w, h = camera      					 # t = top, w = width, h = height (change this to add up/down scroll)
+	l, t, _, _ = -l+HALF_WIDTH, -t, w, h 		 # center player
+
+	l = min(0, l)                             	 # stop scrolling at left edge
+	l = max(-(camera.width - GAME_WIDTH), l)     # stop scrolling at the right edge
+    # t = max(-(camera.height - GAME_HEIGHT), t) # stop scrolling at the bottom
+    # t = min(0, t)                              # stop scrolling at the top
+
+	return Rect(l, t, w, h)
+
+
+##---- CREATE LEVEL OBJECTS - blocks and tokens -----##
+def create_level():
+	global blockList, tokenList
+	blockList = []; 
+	tokenList = []
+	for y in range(0, len(level1)):
+		for x in range(0, len(level1[y])):
+
+			if level1[y][x] == 1: ## stationary blocks
+				blockList.append(Block(x * BLOCK_WIDTH, y * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_SPEED_1))
+
+			if level1[y][x] == 2: ## moving blocks
+				blockList.append(Block(x * BLOCK_WIDTH, y * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_SPEED_2))
+
+			if level1[y][x] == 3: ## moving blocks opposite direction
+				blockList.append(Block(x * BLOCK_WIDTH, y * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT, -BLOCK_SPEED_2))
+
+			if level1[y][x] == 4: ## stationary blocks + token
+				blockList.append(Block(x * BLOCK_WIDTH, y * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_SPEED_1))
+				tokenList.append(Token(x * BLOCK_WIDTH, y * BLOCK_HEIGHT, TOKEN_WIDTH, TOKEN_HEIGHT, BLOCK_SPEED_1))
+
+	blockList = pygame.sprite.Group(blockList)
+	tokenList = pygame.sprite.Group(tokenList)
+
+
+##---- GAME OVER LOOP ----##
+def gameOver():
+	gameOver = True
+	while gameOver:
+
+		## Display choice
+		text = font.render('You died! Space to play again or Esc to quit', 13, (0, 0, 0))
+		textx = GAME_WIDTH / 2 - text.get_width() / 2
+		texty = GAME_HEIGHT / 2 - text.get_height() / 2
+		textx_size = text.get_width()
+		texty_size = text.get_height()
+		pygame.draw.rect(window, (255, 255, 255), ((textx - 5, texty - 5), (textx_size + 10, texty_size + 10)))
+		window.blit(text, (GAME_WIDTH / 2 - text.get_width() / 2, GAME_HEIGHT / 2 - text.get_height() / 2))
+
+		## Check player's keyboard input
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+				pygame.display.quit()
+				pygame.quit()
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+				gameOver = False
+
+		clock.tick(FRAME_RATE)
+		pygame.display.flip()
+
+	game_loop()
+
+
+##----- PAUSE MENU ------##
+def pause():
+	paused = True
+	while paused:
+
+		## Display choice
+		text = font.render('Game paused! Space to continue or Esc to quit', 13, (0, 0, 0))
+		textx = GAME_WIDTH / 2 - text.get_width() / 2
+		texty = GAME_HEIGHT / 2 - text.get_height() / 2
+		textx_size = text.get_width()
+		texty_size = text.get_height()
+		pygame.draw.rect(window, (255, 255, 255), ((textx - 5, texty - 5), (textx_size + 10, texty_size + 10)))
+		window.blit(text, (GAME_WIDTH / 2 - text.get_width() / 2, GAME_HEIGHT / 2 - text.get_height() / 2))
+
+		## Check player's keyboard input
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+				pygame.display.quit()
+				pygame.quit()
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+				paused = False
+
+		clock.tick(FRAME_RATE)
+		pygame.display.flip()
+
+
+##------ GAME LOOP ------##
+def game_loop():
+
+	## Set variables
+	loopCount = 0                    
+	inputMap = [False, False, False] ## For key mapping
+	gameLoop = True
+	paused = False
+
+	## Create objects
+	camera = Camera(complex_camera, GAME_WIDTH * N_SCREENS, GAME_HEIGHT * N_SCREENS) ## Instance of camera
+	player = Player(START_X, START_Y, BLACK, PLAYER_WIDTH, PLAYER_HEIGHT)            ## Create player object
+	create_level()																	 ## Create level
+
+	## Loop
+	while gameLoop:
+		loopCount += 1 ## increment loop counter
+
+		## Event loops
+		for event in pygame.event.get():
+
+			if (event.type == pygame.QUIT):
+				pygame.display.quit()
+				pygame.quit()
+
+			## Key pressed down
+			inputMap[2] = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_d:
+					inputMap[0] = True
+				elif event.key == pygame.K_a:
+					inputMap[1] = True
+				elif event.key == pygame.K_w:
+					inputMap[2] = True
+				elif event.key == pygame.K_SPACE:
+					paused = True
+					pause()
+
+			## Key released
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_d:
+					inputMap[0] = False
+				elif event.key == pygame.K_a:
+					inputMap[1] = False
+
+		window.fill(BLUE)     ## Fill window
+		camera.update(player) ## Update camera
+
+		## Update blocks
+		for block in blockList:
+			block.update(loopCount)
+
+		## Update player
+		player.update(blockList, inputMap, tokenList, loopCount)
+
+		## Draw everything
+		for block in blockList:                            ## blocks
+			window.blit(block.image, camera.apply(block))
+		for token in tokenList:                            ## tokens
+			window.blit(token.image, camera.apply(token))
+		window.blit(player.image, camera.apply(player))    ## player
+
+		## Display player's health
+		strHealth = "Health: " + str(player.health)
+		text = font.render(strHealth, 1, (FONT_X, FONT_Y, 0))
+		textPos = text.get_rect()
+		window.blit(text, textPos)
+
+		## Display restart option if player dies
+		if player.health == 0:
+			gameOver()
+
+		clock.tick(FRAME_RATE)
+		pygame.display.flip()
+
+game_loop() ## RUN GAME
